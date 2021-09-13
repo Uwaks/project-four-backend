ac068b5ed3c3e2e530768030ea16e54766b13641
maggieliz <maggielizward@gmail.com>
2021-09-13 10:08:57 +0100

    Made profile image an optional field on user model.

---
+++ jwt_auth/management/commands/createdata.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+class Command(BaseCommand):
+    help = "Command information"
+
+    def handle(self, *args, **kwargs):
+        
+        fake = Faker()
+
+        print(fake.name())
--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -1,10 +1,11 @@
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
     email = models.CharField(max_length=50)
-    profile_image = models.CharField(max_length=300)
+    profile_image = models.CharField(max_length=300, blank=True)
     following = models.ManyToManyField(
         'jwt_auth.User',
         related_name = 'followed_by',
